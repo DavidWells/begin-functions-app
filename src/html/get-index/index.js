d915ef049480f557c1b86de1839ede564444de4a
David Wells <hello@davidwells.io>
2018-10-24 14:51:01 -0700

    Update index.js

--- src/html/get-index/index.js
+++ src/html/get-index/index.js
@@ -13,7 +13,7 @@ let defaultHTML = `
 
     <h1 class="center-text">
       <!-- ↓ Change "Hello world!" to something else and head on back to Begin! -->
-      Hello world!
+      Hello GLOBE
     </h1>
 
     <p class="center-text">
